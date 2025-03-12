b64984853c1b80d91b4d9ce03453526bb4716bbf
gematik-Entwicklung <software-development@gematik.de>
2025-03-12 16:09:14 +0100

    Release 2.4.1

---
+++ src/main/java/de/gematik/demis/service/base/apidoc/DefaultApiSpecConfig.java
@@ -0,0 +1,42 @@
+package de.gematik.demis.service.base.apidoc;
+
+/*-
+ * #%L
+ * service-base
+ * %%
+ * Copyright (C) 2025 gematik GmbH
+ * %%
+ * Licensed under the EUPL, Version 1.2 or - as soon they will be approved by the
+ * European Commission – subsequent versions of the EUPL (the "Licence").
+ * You may not use this work except in compliance with the Licence.
+ *
+ * You find a copy of the Licence in the "Licence" file or at
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either expressed or implied.
+ * In case of changes by gematik find details in the "Readme" file.
+ *
+ * See the Licence for the specific language governing permissions and limitations under the Licence.
+ * #L%
+ */
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DefaultApiSpecConfig {
+
+  @Bean
+  public OpenAPI customOpenAPI(
+      @Value("${info.app.name}") String appName, @Value("${info.app.version}") String appVersion) {
+    return new OpenAPI()
+        .addServersItem(new Server().url(""))
+        .info(new Info().title(appName).version(appVersion));
+  }
+}
